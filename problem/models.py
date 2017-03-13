a0ae15ea6e4c9f483fe9a2ea45ac59bb9346f596
zerol <zerolfx0@gmail.com>
2017-03-13 18:42:48 +0800

    add: problem model add: xss_filter in utils

--- problem/models.py
+++ problem/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from account.models import User
 
-# Create your models here.
+
+class Problem(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField()
+    created_by = models.ForeignKey(User)
+
+    visible = models.BooleanField(default=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    time_limit = models.IntegerField
+    memory_limit = models.IntegerField
+
+    total_submit_number = models.IntegerField(default=0)
+    total_accepted_number = models.IntegerField(default=0)
