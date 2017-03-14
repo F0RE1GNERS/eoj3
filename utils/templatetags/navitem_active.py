595015dae9f0fb4924c30606a85664541351267f
ultmaster <scottyugochang@hotmail.com>
2017-03-14 15:15:48 +0800

    add: [navitem active] get request from context

--- utils/templatetags/navitem_active.py
+++ utils/templatetags/navitem_active.py
@@ -3,20 +3,18 @@ from django.core.urlresolvers import reverse, resolve
 
 register = template.Library()
 
-@register.simple_tag
-def navitem_active(request, match):
+@register.simple_tag(takes_context=True)
+def navitem_active(context, match):
     """
-    In template: {% navitem_active request "namespace(optional):name" %}
+    In template: {% navitem_active "namespace(optional):name" %}
     """
-    resolve_path = resolve(request.path)
+    resolve_path = context['request'].resolver_match
     print(resolve_path.namespace)
 
     if ':' in match:
         namespace, name = match.split(':', 1)
     else:
         namespace, name = '', match
-    print(namespace, name)
-    print(resolve_path.url_name)
     if resolve_path.namespace == namespace and resolve_path.url_name.startswith(name):
         return "active"
     return ""
