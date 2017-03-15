56543ee653b656450d4d97aefe12a04845f1ccc2
zerol <zerolfx0@gmail.com>
2017-03-15 15:27:36 +0800

    add: contest list & add & edit

--- backstage/contest/forms.py
+++ backstage/contest/forms.py
@@ -6,3 +6,7 @@ class ContestEditForm(forms.ModelForm):
     class Meta:
         model = Contest
         fields = ['title', 'description', 'start_time', 'end_time', 'visible']
+        help_texts = {
+            'start_time': 'YYYY-MM-DD --:--(:--)',
+            'end_time': 'YYYY-MM-DD --:--(:--)',
+        }
