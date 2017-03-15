1041c94d196c73f139ad743986d8fd16282445e1
zerol <zerolfx0@gmail.com>
2017-03-15 14:52:51 +0800

    merge:

--- backstage/server/views.py
+++ backstage/server/views.py
@@ -1,39 +1,26 @@
 from django.shortcuts import render, HttpResponseRedirect, reverse
-from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
 from django.utils.decorators import method_decorator
 
 from .forms import ServerEditForm
 from dispatcher.models import Server
 
-@method_decorator(login_required(), name='dispatch')
-class ServerCreate(CreateView):
+from ..base_views import BaseCreateView, BaseUpdateView
+
+
+class ServerCreate(BaseCreateView):
     form_class = ServerEditForm
     template_name = 'backstage/server/server_add.html'
 
-    def form_valid(self, form):
-        instance = form.save(commit=False)
-        instance.created_by = self.request.user
-        instance.save()
-        messages.add_message(self.request, messages.SUCCESS, "Server was successfully added.")
-        return HttpResponseRedirect(reverse('backstage:server'))
+    def get_redirect_url(self, instance):
+        return reverse('backstage:server')
 
 
-@method_decorator(login_required(), name='dispatch')
-class ServerUpdate(UpdateView):
+class ServerUpdate(BaseUpdateView):
     form_class = ServerEditForm
     template_name = 'backstage/server/server_edit.html'
     queryset = Server.objects.all()
-
-    def get_success_url(self):
-        return self.request.path
-
-    def form_valid(self, form):
-        form.save()
-        messages.add_message(self.request, messages.SUCCESS, "Your changes have been saved.")
-        return HttpResponseRedirect(self.get_success_url())
 
 
 class ServerList(ListView):
