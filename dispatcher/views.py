15e5d731ba86a6cd60d64792a30c678a6171df3b
ultmaster <scottyugochang@hotmail.com>
2017-03-16 20:43:46 +0800

    add: submission page

--- dispatcher/views.py
+++ dispatcher/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
 import requests
 import os
 import threading
+import json
 
 from .models import ServerProblemStatus, Server
 from problem.models import Problem
-from submission.models import Submission
+from submission.models import Submission, SubmissionStatus
 from eoj3.settings import TESTDATA_DIR
 from utils.url_formatter import upload_linker, judge_linker
 
@@ -32,7 +34,7 @@ class Dispatcher:
 
     def update_data_for_server(self):
         if self.is_latest_data_for_server():
-            return
+            return True
         try:
             file_path = os.path.join(TESTDATA_DIR, str(self.problem.pk) + '.zip')
             with open(file_path, 'rb') as f:
@@ -42,31 +44,56 @@ class Dispatcher:
                 if response['status'] != 'received':
                     raise ConnectionError('Remote server rejected the request.')
                 self.status.testdata_hash = self.problem.testdata_hash
+                return True
+        except FileNotFoundError:
+            print('Data file not found. Why?')
+            return True
         except Exception as e:
             print('Something wrong during update:')
             print(e)
+            return False
 
     def dispatch(self):
-        self.update_data_for_server()
-        self.server.save()
-        request = {
-            "id": self.submission.pk,
-            "lang": self.submission.lang,
-            "code": self.submission.code,
-            "settings": {
-                "max_time": self.problem.time_limit,
-                "max_sum_time": self.problem.sum_time_limit,
-                "max_memory": self.problem.memory_limit,
-                "problem_id": self.problem.pk
-            },
-            "judge": self.problem.judge
-        }
         try:
+            self.update_data_for_server()
+            self.server.save()
+            self.submission.judge_start_time = timezone.now()
+            self.submission.status = SubmissionStatus.JUDGING
+            self.submission.save()
+            request = {
+                "id": self.submission.pk,
+                "lang": self.submission.lang,
+                "code": self.submission.code,
+                "settings": {
+                    "max_time": self.problem.time_limit,
+                    "max_sum_time": self.problem.sum_time_limit,
+                    "max_memory": self.problem.memory_limit,
+                    "problem_id": self.problem.pk
+                },
+                "judge": self.problem.judge
+            }
+            # Request: wait for one hour
             response = requests.post(judge_linker(self.server.ip, self.server.port),
-                                     json=request, auth=('token', self.server.token)).json()
+                                     json=request, auth=('token', self.server.token),
+                                     timeout=3600).json()
+            # If presented multiple judge responses at the same time, unexpected results will happen.
+            if response['status'] != 'received':
+                raise ConnectionError('Remote server rejected the request.')
             print(response)
+            self.submission.judge_end_time = timezone.now()
+            self.submission.status = response['verdict']
+            if self.submission.status == SubmissionStatus.COMPILE_ERROR:
+                self.submission.status_detail = response['message']
+            else:
+                self.submission.status_detail = json.dumps(response['detail'])
+                self.submission.status_time = response['time']
+                self.submission.status_memory = response['memory']
+            self.submission.save()
+            return True
         except Exception as e:
-            pass
+            print('Something wrong during dispatch:')
+            print(e)
+            return False
 
 
 class DispatcherThread(threading.Thread):
@@ -77,9 +104,13 @@ class DispatcherThread(threading.Thread):
         self.submission = submission
 
     def run(self):
-        Dispatcher(self.problem_id, self.submission).dispatch()
-
-
-def test():
-    sub = Submission(lang='cpp', code='int main() { return 0; }')
-    Dispatcher(1, 'ccc').update_data_for_server()
+        server_count = len(Server.objects.all())
+        count = 0
+        while True:
+            if count > server_count * 2:
+                self.submission.status = SubmissionStatus.SYSTEM_ERROR
+                self.submission.save()
+                break
+            if Dispatcher(self.problem_id, self.submission).dispatch():
+                break
+            count += 1
--- submission/views.py
+++ submission/views.py
@@ -1,6 +1,22 @@
+import json
 from django.shortcuts import render
-from .models import Submission
+from .models import Submission, get_color_from_status
 
 def submission_view(request, pk):
     submission = Submission.objects.get(pk=pk)
-    return render(request, 'submission.html', context={'submission': submission})
+    context = {'submission': submission}
+    try:
+        detail_msg = submission.status_detail
+        if detail_msg == '':
+            raise ValueError
+        detail = json.loads(detail_msg)
+        for d in detail:
+            d['color'] = get_color_from_status(d['verdict'])
+        detail.sort(key=lambda x: x['count'])
+        print(detail)
+        context.update({'detail': detail})
+    except ValueError:
+        pass
+    except Exception as e:
+        print(e)
+    return render(request, 'submission.html', context=context)
