0b87e9a0ec82da82a562891c769001370d377216
zerol <zerolfx0@gmail.com>
2017-03-16 23:54:36 +0800

    add: code highlight

--- submission/views.py
+++ submission/views.py
@@ -1,11 +1,15 @@
 import json
 from django.shortcuts import render
 from .models import Submission, get_color_from_status
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
 
 
 def submission_view(request, pk):
     submission = Submission.objects.get(pk=pk)
-    context = {'submission': submission}
+    context = dict(submission=submission)
+    context['code'] = highlight(submission.code, get_lexer_by_name(submission.lang), HtmlFormatter())
     try:
         detail_msg = submission.status_detail
         if detail_msg == '':
