64edb8bb08971585083877b8d893ca2273950db8
zerol <zerolfx0@gmail.com>
2017-03-18 19:03:39 +0800

    add: link_active

--- contest/views.py
+++ contest/views.py
@@ -25,7 +25,10 @@ class BaseContestMixin(TemplateResponseMixin, ContextMixin):
         data['remaining_time'] = "%d:%.2d:%.2d" % (remaining_time_seconds // 3600,
                                                    remaining_time_seconds % 3600 // 60,
                                                    remaining_time_seconds % 60)
-        data['contest_problem_list'] = get_list_or_404(ContestProblem, contest=data['contest'])
+        contest_problem_list = get_list_or_404(ContestProblem, contest=data['contest'])
+        for contest_problem in contest_problem_list:
+            contest_problem.title = "%s. %s" % (contest_problem.identifier, contest_problem.problem.title)
+        data['contest_problem_list'] = contest_problem_list
         return data
 
 
--- utils/templatetags/navitem_active.py
+++ utils/templatetags/navitem_active.py
@@ -1,6 +1,7 @@
 from django import template
 from django.core.urlresolvers import reverse, resolve
-
+from django.shortcuts import reverse
+from django.utils.html import format_html
 register = template.Library()
 
 
@@ -21,3 +22,32 @@ def navitem_active(context, match):
             return "active"
     else:
         return ""
+
+
+@register.simple_tag(takes_context=True)
+def link_active(context, view_name, text, *args, **kwargs):
+    if kwargs.pop('outside', None):
+        html = r"""
+            <li class="nav-item {active}">
+                <a class="nav-link" href="{url}">{text}</a>
+            </li>
+            """
+    else:
+        html = '<a href="{url}" class="{active}">{text}</a>'
+    active = False
+    resolve_path = context['request'].resolver_match
+    url = reverse(view_name, args=args)
+    if args:
+        if url == context['request'].path:
+            active = True
+    else:
+        if ':' in view_name:
+            namespace, name = view_name.split(':', 1)
+            if resolve_path.namespace == namespace and resolve_path.url_name.startswith(name):
+                active = True
+        elif '/' in view_name:
+            if context['request'].path == view_name:
+                active = True
+        elif resolve_path.url_name.startswith(view_name) and not resolve_path.namespace or resolve_path.namespace == view_name:
+                active = True
+    return format_html(html, url=url, active="active" if active else "", text=text)
