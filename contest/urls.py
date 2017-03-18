e07fa11a95b1bbfc503d5bdd687294a68c8f0c67
ultmaster <scottyugochang@hotmail.com>
2017-03-19 01:01:15 +0800

    add: contest status

--- contest/urls.py
+++ contest/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
 from .views import ContestList, ContestProblemDetail, ContestStandings, ContestSubmit, DashboardView
-from .views import ContestMySubmission
+from .views import ContestMySubmission, ContestStatus
 
 urlpatterns = [
     url(r'^$', ContestList.as_view(), name='list'),
@@ -10,4 +10,5 @@ urlpatterns = [
     url(r'^(?P<cid>\d+)/problem/(?P<pid>\w+)/$', ContestProblemDetail.as_view(), name='problem'),
     url(r'^(?P<cid>\d+)/submit/$', ContestSubmit.as_view(), name='submit'),
     url(r'^(?P<cid>\d+)/submission/$', ContestMySubmission.as_view(), name='submission'),
+    url(r'^(?P<cid>\d+)/status/$', ContestStatus.as_view(), name='status'),
 ]
