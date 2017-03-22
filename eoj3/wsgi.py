39a5ff93568e101a0902e42c6903294a0589cddc
ultmaster <scottyugochang@hotmail.com>
2017-03-23 00:04:45 +0800

    fix: wsgi

--- eoj3/wsgi.py
+++ eoj3/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-
+from os.path import join, dirname, abspath
 from django.core.wsgi import get_wsgi_application
+
+PROJECT_DIR = dirname(dirname(abspath(__file__)))
+import sys
+sys.path.insert(0, PROJECT_DIR)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eoj3.settings")
 
