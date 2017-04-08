bfd3e73b9498d7bb75683ed5e202f681ed3eb137
ultmaster <scottyugochang@hotmail.com>
2017-04-08 11:13:37 +0800

    fix: too long abstract

--- utils/jinja2/filters.py
+++ utils/jinja2/filters.py
@@ -68,4 +68,4 @@ def sample_format(value):
 @library.filter(name='get_intro')
 def get_intro(value):
     soup = BeautifulSoup(value, "html.parser")
-    return soup.get_text(' ')[:2048]
+    return soup.get_text(' ')[:256]
