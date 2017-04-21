dd245e320137e281ce46a83b5178cec199ab3eaa
zerol <zerolfx0@gmail.com>
2017-04-22 00:25:20 +0800

    fix: efficiency with tags

--- problem/views.py
+++ problem/views.py
@@ -8,7 +8,7 @@ from django.utils.decorators import method_decorator
 from django.contrib import messages
 from django.http.response import Http404
 from django.core.exceptions import PermissionDenied
-from tagging.models import Tag, TaggedItem
+from tagging.models import Tag, TaggedItem, ContentType
 
 from .models import Problem
 from submission.forms import SubmitForm
@@ -42,16 +42,18 @@ class ProblemList(ListView):
                 tag = Tag.objects.filter(name=kw)
                 if tag.exists():
                     queryset = (queryset | TaggedItem.objects.get_by_model(Problem, tag.first())).distinct()
-        return queryset.all()
+
+        return queryset
 
     def get_context_data(self, **kwargs):
         data = super(ProblemList, self).get_context_data(**kwargs)
         data['keyword'] = self.request.GET.get('keyword')
-        data['show_tags'] = True
+        show_tags = True
+        current_problem_set = [problem.pk for problem in data['problem_list']]
         if self.request.user.is_authenticated:
+            show_tags = self.request.user.show_tags
             # Get AC / Wrong
-            current_problem_set = [problem.pk for problem in data['problem_list']]
-            submission_list = self.request.user.submission_set.only('pk', 'author_id', 'problem_id', 'status').\
+            submission_list = self.request.user.submission_set.only('pk', 'author_id', 'problem_id', 'status'). \
                 filter(problem_id__in=current_problem_set).all()
             submission_set = dict()
             for submission in submission_list:
@@ -63,8 +65,15 @@ class ProblemList(ListView):
             for problem in data['problem_list']:
                 if submission_set.get(problem.pk) is not None:
                     problem.status = submission_set.get(problem.pk)
-            # Get tags
-            data['show_tags'] = self.request.user.show_tags
+
+        # Get tags
+        if show_tags:
+            tagged_items = list(TaggedItem.objects.filter(content_type=ContentType.objects.get_for_model(Problem))
+                                .filter(object_id__in=current_problem_set))
+            for problem in data['problem_list']:
+                items = list(filter(lambda x: x.object_id == problem.pk, tagged_items))
+                if items:
+                    problem.my_tags = map(lambda x: x.tag.name, items)
 
         return data
 
