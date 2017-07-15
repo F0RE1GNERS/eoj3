c69be079902d38b880f2c8270506df6508667e73
ultmaster <scottyugochang@hotmail.com>
2017-07-15 10:37:22 +0800

    checkpoint

---
+++ static/js/type.js
@@ -0,0 +1,14 @@
+new Typed('.typed-element', {
+  strings: ["Start coding now.", "#include <bits/stdc++.h>", "class Main {",
+            "a, b = map(int, input().split())", "An AC a day keeps WA away.", "Thousands of problems.",
+            "OI/ACM support.", "Blog as you want.", "It is more than coding."],
+  typeSpeed: 50,
+  backSpeed: 5,
+  startDelay: 500,
+  backDelay: 2000,
+  loop: true,
+  showCursor: true,
+  cursorChar: '|',
+  autoInsertCss: true,
+  contentType: 'null'
+});
