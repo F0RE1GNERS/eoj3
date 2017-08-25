7b486ccf269314a3105db17fa0e8d61e42711398
ultmaster <scottyugochang@hotmail.com>
2017-08-25 23:59:40 +0800

    fix: migrate spj

--- migrate/migrate_spj.py
+++ migrate/migrate_spj.py
@@ -3,14 +3,20 @@ import traceback
 
 
 def run():
+    # try:
+    #     for problem in Problem.objects.all():
+    #         if problem.judge == '' or problem.judge == 'fcmp':
+    #             continue
+    #         if SpecialProgram.objects.filter(filename__contains=problem.judge).exists():
+    #             problem.checker = SpecialProgram.objects.filter(filename__contains=problem.judge).first().fingerprint
+    #         else:
+    #             problem.visible = False
+    #         problem.save(update_fields=['checker', 'visible'])
+    # except:
+    #     traceback.print_exc()
     try:
         for problem in Problem.objects.all():
-            if problem.judge == '' or problem.judge == 'fcmp':
-                continue
-            if SpecialProgram.objects.filter(filename__contains=problem.judge).exists():
-                problem.checker = SpecialProgram.objects.filter(filename__contains=problem.judge).first().fingerprint
-            else:
-                problem.visible = False
-            problem.save(update_fields=['checker', 'visible'])
+            if not SpecialProgram.objects.filter(fingerprint=problem.checker).exists():
+                print(problem.checker)
     except:
         traceback.print_exc()
