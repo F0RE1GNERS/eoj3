08ab8a7a8d5bcb2d3b8367f3139b8eefe21d39ea
ultmaster <scottyugochang@hotmail.com>
2017-08-27 08:32:12 +0800

    update: captcha style

--- eoj3/captcha.py
+++ eoj3/captcha.py
@@ -2,14 +2,15 @@ from random import randint, choice
 
 
 def random_math_challenge():
-    a, b = randint(1, 10), randint(1, 9)
-    c = choice('+-*/')
+    a = randint(1, 10)
+    b = randint(1, a)
+    a /= 10
+    b /= 10
+    c = choice('+-*')
     if c == '+':
         d = a + b
     elif c == '-':
         d = a - b
-    elif c == '*':
-        d = a * b
     else:
-        d = a // b
-    return '%d%s%d=' % (a, c, b), str(d)
+        d = a * b
+    return '%.1f%s%.1f' % (a, c, b), str(d)
