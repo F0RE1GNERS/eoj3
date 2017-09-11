37defad3fe26487e91b2ba324bf0cd9738ac20c5
ultmaster <scottyugochang@hotmail.com>
2017-09-11 20:13:56 +0800

    fix: better support for clarification of admins

--- contest/clarification.py
+++ contest/clarification.py
@@ -1,18 +1,11 @@
-from django.shortcuts import HttpResponseRedirect, reverse, redirect, get_object_or_404
-from django.views.generic.list import ListView
-from django.views.generic import View
-from django.utils import timezone
 from django.core.exceptions import PermissionDenied
-from django.http import HttpResponse
-from django.contrib import messages
-from django.db.models import Q
-import json
-import datetime
+from django.shortcuts import reverse, redirect, get_object_or_404
+from django.views.generic import View
+from notifications.signals import notify
 
 from utils.permission import has_permission_for_contest_management
-from .models import Contest, ContestClarification
+from .models import ContestClarification
 from .views import BaseContestMixin
-from account.permissions import is_admin_or_root
 
 
 class ContestClarificationView(BaseContestMixin, View):
@@ -25,8 +18,18 @@ class ContestClarificationView(BaseContestMixin, View):
             raise PermissionDenied
         if has_permission_for_contest_management(request.user, self.contest):
             ContestClarification.objects.create(contest=self.contest, important=True, author=request.user, answer=text)
+            notify.send(sender=request.user,
+                        recipient=list(map(lambda x: x.user, self.contest.contestparticipant_set.select_related("user").all())),
+                        verb="posted a notification in",
+                        level="warning",
+                        target=self.contest)
         else:
             ContestClarification.objects.create(contest=self.contest, author=request.user, text=text)
+            notify.send(sender=request.user,
+                        recipient=self.contest.manager.all(),
+                        verb="asked a question in",
+                        level="warning",
+                        target=self.contest)
         return redirect(reverse("contest:dashboard", kwargs={"cid": self.contest.pk}))
 
 
@@ -37,5 +40,10 @@ class ContestClarificationAnswer(BaseContestMixin, View):
             clarification = get_object_or_404(ContestClarification, contest_id=cid, pk=pk)
             clarification.answer = request.POST["text"]
             clarification.save(update_fields=["answer"])
+            notify.send(sender=request.user,
+                        recipient=[clarification.author],
+                        verb="answered a question in",
+                        level="warning",
+                        target=self.contest)
             return redirect(reverse("contest:dashboard", kwargs={"cid": self.contest.pk}))
         raise PermissionDenied
