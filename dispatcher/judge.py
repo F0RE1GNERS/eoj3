85dbc9feb88da278e111c55cd3d6fb3d3f299305
ultmaster <scottyugochang@hotmail.com>
2018-05-07 07:43:25 +0800

    fix: increase timeout

--- dispatcher/judge.py
+++ dispatcher/judge.py
@@ -13,7 +13,7 @@ from .manage import DEFAULT_USERNAME
 
 
 def send_judge_through_http(server, code, lang, max_time, max_memory, run_until_complete, cases, checker,
-                            interactor, callback, timeout=1200):
+                            interactor, callback, timeout=1800):
     server.last_seen_time = datetime.now()
     server.save(update_fields=['last_seen_time'])
     data = _prepare_judge_json_data(code, lang, max_time, max_memory, run_until_complete, cases, checker, interactor)
@@ -26,7 +26,7 @@ def send_judge_through_http(server, code, lang, max_time, max_memory, run_until_
 
 
 def send_judge_through_watch(server, code, lang, max_time, max_memory, run_until_complete, cases, checker,
-                             interactor, callback, timeout=600, fallback=True, report_file_path=None):
+                             interactor, callback, timeout=900, fallback=True, report_file_path=None):
     """
     :param interactor: None or '' if there is no interactor
     :param callback: function, to call when something is returned (possibly preliminary results)
