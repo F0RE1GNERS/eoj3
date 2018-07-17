cfe865371b0f3328b9eea07e5aefd98bbdd2464b
ultmaster <scottyugochang@hotmail.com>
2018-07-17 14:37:49 +0800

    fix: create time filter

--- problem/tasks.py
+++ problem/tasks.py
@@ -1,6 +1,6 @@
 import random
 from collections import Counter
-from datetime import datetime
+from datetime import datetime, timedelta
 from threading import Thread
 
 from django import db
@@ -58,7 +58,9 @@ def create_submission(problem, author: User, code, lang, contest=None, status=Su
         datetime.now() - author.submission_set.first().create_time).total_seconds() < settings.SUBMISSION_INTERVAL_LIMIT:
         raise ValueError("Please don't resubmit in 5 seconds.")
     if contest:
-        if contest.submission_set.filter(author=author, problem_id=problem).count() >= settings.SUBMISSION_ATTEMPT_LIMIT:
+        if contest.submission_set.filter(author=author, problem_id=problem,
+                                         create_time__gt=datetime.now() - timedelta(days=1)).count() \
+                >= settings.SUBMISSION_ATTEMPT_LIMIT:
             raise ValueError("Running out of attempts.")
         if contest.submission_set.filter(author=author, problem_id=problem, code=code, lang=lang).exists():
             raise ValueError("You have submitted exactly the same code before.")
