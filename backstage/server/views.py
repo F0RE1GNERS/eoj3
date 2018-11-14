b02e3874253dee15f695deb464c7ff3b85aa4c84
ultmaster <scottyugochang@hotmail.com>
2018-11-14 10:11:55 +0800

    last_seen -> last_seen_time

--- backstage/server/views.py
+++ backstage/server/views.py
@@ -45,7 +45,7 @@ class ServerUpdate(BaseUpdateView):
 
 class ServerList(BaseBackstageMixin, ListView):
     template_name = 'backstage/server/server.jinja2'
-    queryset = Server.objects.all().order_by("-last_seen")
+    queryset = Server.objects.all().order_by("-last_seen_time")
     context_object_name = 'server_list'
 
     def get_context_data(self, **kwargs):
