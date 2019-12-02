a00f911cac6573b950a55a1866378ed99ced38db
jxtxzzw <jxtxzzw@126.com>
2019-12-02 14:21:24 +0800

    修改迁移页面的表达（Issued by Xiejiadong） (#51)
    将迁移页面 “用户名” 修改成 “你想要注销掉的用户名”，避免歧义
    （Xiejiadong: 最近接了 差不多十个 迁移反的。。

--- migrate/forms.py
+++ migrate/forms.py
@@ -7,6 +7,6 @@ class MigrateForm(forms.Form):
     ('old', '在 2017 年二月之前在旧 EOJ 中注册'),
     ('new', '在 2017 年五月之后在新 EOJ 中注册')
   ))
-  username = forms.CharField(label='用户名', max_length=224, required=True)
-  password = forms.CharField(label='密码', widget=forms.PasswordInput, max_length=224, required=True)
+  username = forms.CharField(label='你想要注销掉的用户名', max_length=224, required=True)
+  password = forms.CharField(label='那个账户的密码', widget=forms.PasswordInput, max_length=224, required=True)
   captcha = CaptchaField(label="小学数学题")
