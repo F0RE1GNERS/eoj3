029eda00c174c741d1730a9d6bac9cf21c193e29
Zihan Wang <wangzihan@live.com>
2021-12-16 10:57:39 +0800

    pylint

--- utils/csv_writer.py
+++ utils/csv_writer.py
@@ -14,6 +14,7 @@ def write_csv(data):
   file_name = random_string()
   file_path = path.join(settings.GENERATE_DIR, file_name)
   with open(file_path, 'w', newline='', encoding='utf-8-sig') as csvfile:
+  # csv files need to be encoded using UTF-8 BOM to be correctly opened in Excel on Windows
     writer = csv.writer(csvfile)
     for d in data:
       writer.writerow(d)
